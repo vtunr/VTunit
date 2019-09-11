e88e4cede04fa562acad1f4881a2171b7aac137b
Tony <tony@foobot.io>
2019-09-11 21:24:19 +0200

    Adding running one specific test

--- vtunit.py
+++ vtunit.py
@@ -4,7 +4,9 @@ import argparse
 from cmakelist_generator import *
 from file_generator import *
 import subprocess
+from subprocess import check_output
 import shutil
+
 class Project:
     def __init__(self):
         self.current_folder = os.getcwd()
@@ -47,13 +49,41 @@ class Project:
         os.chdir("build")
         subprocess.call(self.cmd_cmake, shell=True)
         os.chdir("../")
-
-    def run(self):
+    def list_test(self):
         os.chdir("build")
-        subprocess.call(self.cmd_ninja, shell=True)
-        subprocess.call(self.cmd_ctest, shell=True)
-        subprocess.call(self.cmd_gen_xml, shell=True)
+        cmd = "ctest -N".split(" ")
+        out = check_output(cmd)
+        list_test = []
+        for i in out.split("\n"):
+            if("Test #" in i):
+                splt = i.split(": ")[1]
+                list_test.append(splt)
         os.chdir("../")
+        return list_test
+    
+    def print_test_list(self):
+        list_test = self.list_test()
+        len_test = len(list_test)
+        print("%u test found :"%len_test)
+        for test in list_test:
+            print("\t test_%s"%test)
+
+    def run(self, filter):
+        if(filter != None):
+            list_test = self.list_test()
+            if(filter[5:] not in list_test):
+                raise Exception("Test not found")
+            os.chdir("build")
+            subprocess.call(self.cmd_ninja+" test_"+filter, shell = True)
+            subprocess.call(self.cmd_ctest+" -R "+filter[5:], shell=True)
+            subprocess.call(self.cmd_gen_xml, shell=True)
+            os.chdir("../")
+        else:
+            os.chdir("build")
+            subprocess.call(self.cmd_ninja, shell=True)
+            subprocess.call(self.cmd_ctest, shell=True)
+            subprocess.call(self.cmd_gen_xml, shell=True)
+            os.chdir("../")
 
 
 def main():
@@ -75,6 +105,8 @@ def main():
     build.add_argument('--clean_all', help='Clean all unit test (CMake+Ninja)', action='store_true')
     build.add_argument('--cmake', help='Run cmake', action='store_true')
     build.add_argument('--run', help='Run unit test', action='store_true')
+    build.add_argument('--filter', help='Filter unit test')
+    build.add_argument('--list', help='Filter unit test', action='store_true')
     args = parser.parse_args()
     print(args)
     pr = Project()
@@ -88,11 +120,13 @@ def main():
     if(args.command == "build"):
         if(args.clean):
             pr.clean()
-        if(args.command):
+        if(args.clean_all):
             pr.clean_all()
-        if(args.command):
+        if(args.cmake):
             pr.cmake()
-        if(args.command):
-            pr.run()
+        if(args.run):
+            pr.run(args.filter)
+        if(args.list):
+            pr.print_test_list()
 if __name__ == '__main__':
     main()
