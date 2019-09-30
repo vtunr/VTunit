3696adfcd99401ca98e3cc180de0db36707feebf
Tony <tony@foobot.io>
2019-09-30 23:15:07 +0200

    Adding part of xml generator

--- gen_output.py
+++ gen_output.py
@@ -6,10 +6,11 @@ class Test:
         self.line_array = line_array
         self.file_name = os.path.splitext(os.path.basename(line_array[0]))[0]
         self.test_name = '_'.join(line_array[2].split("_")[1:])
+        self.status = line_array[3].strip()
         if(len(line_array) > 4):
-            self.result = "%s \n\t(%s)"%(line_array[3], ':'.join(line_array[4:]).strip())
+            self.result = "%s \n\t(%s)"%(self.status, ':'.join(line_array[4:]).strip())
         else:
-            self.result = line_array[3].strip()
+            self.result = self.status
         
 class FileTested:
     def __init__(self, name):
@@ -31,6 +32,7 @@ class FilesTested:
         self.test_passed = 0
         self.test_ignored = 0
         self.test_failed = 0
+        self.test_number = 0
 
     def add_file_tested(self, file_name):
         already_there = False
@@ -52,6 +54,7 @@ class FilesTested:
                     self.test_failed +=1
                 if("IGNORE" in line):
                     self.test_ignored +=1
+                self.test_number += 1
                 f.add_test(test)
 
     def print_summary(self):
@@ -66,9 +69,6 @@ class OutputGenerator:
     def __init__(self, file_name):
         self.file_name = file_name
         self.ft = FilesTested()
-        self.test_passed = 0
-        self.test_failed = 0
-        self.test_ignored = 0
 
     def process_file(self):
         print('')
@@ -84,6 +84,59 @@ class OutputGenerator:
                     self.ft.add_test_to_file(line_array)
         self.ft.print_summary()
 
+    def gen_xml(self):
+        with open("report.xml", 'w') as f:
+            f.write("<?xml version='1.0' encoding='utf-8' ?>\n")
+            f.write("<TestRun>\n")
+            if(not self.ft.test_ignored):
+                f.write("\t<IgnoredTests/>\n")
+            else:
+                f.write("\t<IgnoredTests>\n")
+                i = 1
+                for ftested in self.ft.file_tested:
+                    for t in ftested.test:
+                        if t.status == "IGNORE":
+                            f.write("""\t\t<Test id="%u">\n"""%i)
+                            f.write("\t\t\t<Name>%s::%s</Name>\n"%(t.line_array[0],t.test_name))
+                            f.write("\t\t</Test>\n")
+                            i+=1
+                f.write("\t</IgnoredTests>\n")
+            if(not self.ft.test_failed):
+                f.write("\t<FailedTests/>\n")
+            else:
+                f.write("\t<FailedTests>\n")
+                i = 1
+                for ftested in self.ft.file_tested:
+                    for t in ftested.test:
+                        if t.status == "FAIL":
+                            f.write("""\t\t<Test id="%u">\n"""%i)
+                            f.write("\t\t\t<Name>%s::%s</Name>\n"%(t.line_array[0],t.test_name))
+                            f.write("\t\t</Test>\n")
+                            i+=1
+                f.write("\t</FailedTests>\n")
+            if(not self.ft.test_passed):
+                f.write("\t<SuccessfulTests/>\n")
+            else:
+                f.write("\t<SuccessfulTests>\n")
+                i = 1
+                for ftested in self.ft.file_tested:
+                    for t in ftested.test:
+                        if t.status == "PASS":
+                            f.write("""\t\t<Test id="%u">\n"""%i)
+                            f.write("\t\t\t<Name>%s::%s</Name>\n"%(t.line_array[0],t.test_name))
+                            f.write("\t\t</Test>\n")
+                            i+=1
+                f.write("\t</SuccessfulTests>\n")
+            f.write("\t<Statistics>\n")
+            f.write("\t\t<Tests>%u</Tests>\n"%(self.ft.test_number))
+            f.write("\t\t<Ignores>%u</Ignores>\n"%(self.ft.test_ignored))
+            f.write("\t\t<FailuresTotal>%u</FailuresTotal>\n"%(0))
+            f.write("\t\t<Failures>%u</Failures>\n"%(self.ft.test_failed))
+            f.write("\t\t<Errors>%u</Errors>\n"%(0))
+            f.write("\t</Statistics>\n")
+            f.write("</TestRun>\n")
+
 
 g = OutputGenerator(sys.argv[1])
 g.process_file()
+g.gen_xml()
