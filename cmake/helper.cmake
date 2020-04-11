b1bb0367732b219e84854017796ad1ce012ab120
Tony <tony@foobot.io>
2020-04-11 18:57:24 +0200

    Also adding python2

--- cmake/helper.cmake
+++ cmake/helper.cmake
@@ -4,7 +4,7 @@ function(generate_test_runner test_file)
 	string(REPLACE .c _Runner.c RUNNER_NAME ${FILE_NAME})
 	add_custom_command(OUTPUT 
 		test_runner/${RUNNER_NAME}
-	COMMAND python ${CMAKE_SOURCE_DIR}/vtunit/generator/test_runner_generator.py --test_file ${test_file}
+	COMMAND python2 ${CMAKE_SOURCE_DIR}/vtunit/generator/test_runner_generator.py --test_file ${test_file}
 	DEPENDS ${test_file}
 )
 endfunction()
@@ -14,7 +14,7 @@ function(generate_mock test_file)
 	string(REPLACE .c " " FILE_NAME ${FILE_NAME})
 	string(STRIP ${FILE_NAME} FILE_NAME)
 	get_property(dirs DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} PROPERTY INCLUDE_DIRECTORIES)
-	add_custom_target(mock_${FILE_NAME} COMMAND python ../vtunit/generator/mock_generator.py --test_file ${test_file} --mock_prefix mock_ --include ${dirs})
+	add_custom_target(mock_${FILE_NAME} COMMAND python2 ../vtunit/generator/mock_generator.py --test_file ${test_file} --mock_prefix mock_ --include ${dirs})
 	add_dependencies(${FILE_NAME} mock_${FILE_NAME})
 endfunction()
 
