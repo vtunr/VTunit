8fafd151e636e0b41e9b9d82f454ef2987ec7464
Tony <tony@foobot.io>
2020-04-11 15:24:51 +0200

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="VTunit", # Replace with your own username
+    version="0.0.1-alpha",
+    author="Tony Martinet",
+    author_email="tonymartinet@gmail.com",
+    description="Unit test helper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vtunr/VTunit",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)
