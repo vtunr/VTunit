1851a5fcd328e3b5e34a2f8bfad28b9ea6d55471
Tony <tony@foobot.io>
2020-04-26 23:13:36 +0200

    Updating argparse (#18)
    * Updating argparse
    
    * Removing cmake option

--- generator/cmakelist_generator.py
+++ generator/cmakelist_generator.py
@@ -20,7 +20,7 @@ class CMakeListsGenerator():
     def generate_cmakelists(self, name):
         with open(os.path.join(self.test_path, self.name_cmake), 'w') as f:
             f.write("cmake_minimum_required(VERSION 3.17)\n\n")
-            f.write("project(\"%s\")\n"%name)
+            f.write("project(%s)\n"%name)
             f.write("include(${VT_CMAKEHELPER}/ctest.cmake)\n")
             f.write("include(${VT_CMAKEHELPER}/helper.cmake)\n")
             f.write("include(${VT_CMAKEHELPER}/unity_fff.cmake)\n")
