a3ee84f3c24d743c3f05d55a174a3f07414970d4
Tony <tony@foobot.io>
2020-04-26 21:39:01 +0200

    Removing need to install some files in each project (#17)
    * Removing need to install some files in each project, running cmake automatically
    
    * reverting some debug change
    
    * fixing code smell

--- cmake/unity_fff.cmake
+++ cmake/unity_fff.cmake
@@ -1,6 +1,6 @@
 #Building unity lib
-add_library(unity STATIC vtunit_files/lib/unity/src/unity.c)
-target_include_directories(unity PUBLIC vtunit_files/lib/unity/src)
+add_library(unity STATIC ${VT_LIB}/unity/src/unity.c)
+target_include_directories(unity PUBLIC ${VT_LIB}/unity/src)
 
 #Including fff.h to our build
-include_directories(vtunit_files/lib/fff/)
+include_directories(${VT_LIB}/fff/)
